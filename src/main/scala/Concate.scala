1759943f369bb0128fa4694960198c346db1c845
anshika-agrawal <anshika18.aa@gmail.com>
2019-07-19 11:27:38 +0530

    in progress..

--- src/main/scala/Concate.scala
+++ src/main/scala/Concate.scala
@@ -1,8 +1,9 @@
 class Concate {
-  def concateList[A](l1:List[A],l2:List[A]):List[A]={
-    case _ if (l2.isEmpty)=> l1
-    case _ if (l1.isEmpty)=> l2
-    case _ => l1::l2
+  def concateList[A](l1: List[A], l2: List[A]): List[A] = {
+    l1 match {
+      case _ if (l2.isEmpty) => l1
+      case _ if (l1.isEmpty) => l2
+      case _ => concateList(l2.head :: l1, l2.tail)
+    }
   }
-
 }
